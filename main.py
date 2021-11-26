8a3862454f2a2b30f41bf62f084d3570bc5e252f
Lopes-Kolekto <lopes@kolekto.com.br>
2021-11-26 13:01:04 -0300

    Abrir explorer quando o programa fechar

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import baseController
+import os
+
 
 choice = "s"
 while (choice.lower() == "s"):
     baseController.baseControllerMain()
     choice = str(input("Continuar?(s/n): "))
+path = os.path.expanduser("~/Desktop/YoutubeDumper/")
+path = os.path.realpath(path)
+os.startfile(path)
